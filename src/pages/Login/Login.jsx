5d1650528676666f4fc48cf71cdd384fb1be7a75
bikash-dev <bikash.dev.bk@gmail.com>
2023-10-08 01:38:16 +0600

    lofin page design

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,68 @@
+
+import { Link } from "react-router-dom";
+import { FcGoogle } from 'react-icons/fc';
+
+
 const Login = () => {
-    return (
-        <div className="max-w-7xl mx-auto">
-            <h2>This is Login</h2>
+
+  return (
+    <div className="hero bg-base-200 pt-12 pb-32">
+      <div className="hero-content flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-3xl md:text-5xl font-bold mb-2">Please Login Here</h1>
         </div>
-    );
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 pb-6">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <Link href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </Link>
+              </label>
+            </div>
+            
+            <div className="form-control mt-6">
+              <button className="btn btn-outline border-none bg-[#ff8080] hover:bg-[#f16c6c] text-white">Login</button>
+            </div>
+            <p className="text-center font-semibold">Or</p>
+            <button
+              className="btn btn-outline border-none bg-[#ff8080] hover:bg-[#f16c6c] text-white"
+            >
+              <FcGoogle /> <span className="ml-2">Sign up with Google</span>{" "}
+            </button>
+          </form>
+          <p className="text-center">
+            New in Dream Wedding?
+            <Link to="/register" className="text-[#ff8080] ml-2 font-bold">
+              Register
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
