9f8ffca9724d6d716a6305768e2ac7006712011a
bikash-dev <bikash.dev.bk@gmail.com>
2023-10-07 19:51:11 +0600

    banner design

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,9 +1,29 @@
+import banner from "../../../assets/banner.jpg";
+import "./Banner.css";
+
 const Banner = () => {
-    return (
+  return (
+    <div className="flex flex-col lg:flex-row gap-12 items-center p-5 md:p-4 lg:p-2 mt-24 mb-40">
+      <div className="flex-1">
+        <img
+          src={banner}
+          alt=""
+          className="rounded-tl-[100px] rounded-br-[100px] border-4 border-[#8ECDDD]"
+        />
+      </div>
+      <div className="flex-1">
+        <h2 className="text-3xl md:text-6xl ">Creating Your Perfect Wedding Day!</h2>
+        <p className="text-lg text-justify my-8">
+          Crafting Unforgettable Moments: Creating Your Perfect Wedding Day!
+        </p>
         <div>
-            <h2 className="">This is Banner</h2>
+          <button className="text-white text-lg bg-[#ff8080] font-semibold px-10 py-4 rounded-[30px]  reservation_btn">
+            Make Reservation
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
