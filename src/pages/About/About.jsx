27f01207237be0581bd97173870c883de9fd0b8d
bikash-dev <bikash.dev.bk@gmail.com>
2023-10-08 01:19:50 +0600

    about photo update

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,5 @@
 import HeroPages from "../../components/HeroPages/HeroPages";
+import image from '../../assets/about_image.jpg';
 
 const About = () => {
     return (
@@ -11,7 +12,7 @@ const About = () => {
                     <p className="text-lg text-justify">Welcome to Dream Weddings & Events, where your wedding dreams come true. We specialize in crafting unforgettable wedding experiences that reflect your love story and unique style. Our dedicated team is here to make your special day truly magical.</p>
                 </div>
                 <div className="flex md:justify-end flex-1">
-                    <img src="http://elementorpress.com/templatekit-pro/layout49/wp-content/uploads/2022/07/about_image1.jpg" alt="" className="rounded-tr-[60px] rounded-bl-[60px] md:rounded-tr-[100px] md:rounded-bl-[100px] border-4 border-[#8ECDDD] hover:shadow-2xl hover:shadow-[#8ECDDD]" />
+                    <img src={image} alt="" className="rounded-tr-[60px] rounded-bl-[60px] md:rounded-tr-[100px] md:rounded-bl-[100px] border-4 border-[#8ECDDD] hover:shadow-2xl hover:shadow-[#8ECDDD]" />
                 </div>
             </div>
         </div>
