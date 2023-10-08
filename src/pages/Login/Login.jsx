909294da109800104f23fb905d2a6ad3a11faf89
bikash-dev <bikash.dev.bk@gmail.com>
2023-10-08 16:43:07 +0600

    login with email and password and google login in login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,57 @@
-
 import { Link } from "react-router-dom";
-import { FcGoogle } from 'react-icons/fc';
-
+import { FcGoogle } from "react-icons/fc";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Login = () => {
+  const { signIn, signInWithGoogle } = useContext(AuthContext);
+
+  const [success, setSuccess] = useState(false);
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSuccess(false);
+
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    console.log(email, password);
+    setPasswordError("");
+
+    signIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setSuccess(true);
+        form.reset();
+      })
+      .catch((error) => {
+        console.error(error);
+        setPasswordError(error.message);
+      });
+  };
+
+  const handleSignInWithGoogle = () => {
+    signInWithGoogle()
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => console.error(error));
+  };
 
   return (
     <div className="hero bg-base-200 pt-12 pb-32">
       <div className="hero-content flex-col">
         <div className="text-center lg:text-left">
-          <h1 className="text-3xl md:text-5xl font-bold mb-2">Please Login Here</h1>
+          <h1 className="text-3xl md:text-5xl font-bold mb-2">
+            Please Login Here
+          </h1>
         </div>
         <div className=" w-full  rounded-tl-[40px] rounded-br-[40px] md:rounded-tl-[60px] md:rounded-br-[60px] hover:shadow-2xl hover:shadow-[#8ECDDD] shadow-2xl bg-base-100 pb-8">
-          <form className="card-body">
+          <form onSubmit={handleSubmit} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -42,15 +81,29 @@ const Login = () => {
                 </Link>
               </label>
             </div>
-            
+
+            {success && (
+              <p className="text-green-500">
+                Successfully login to the account
+              </p>
+            )}
+
+            {passwordError && (
+              <p className="text-red-500">Wrong password or email</p>
+            )}
+
             <div className="form-control mt-6">
-              <button className="btn btn-outline border-none bg-[#ff8080] hover:bg-[#f16c6c] text-white">Login</button>
+              <button className="btn btn-outline border-none bg-[#ff8080] hover:bg-[#f16c6c] text-white">
+                Login
+              </button>
             </div>
             <p className="text-center font-semibold">Or</p>
             <button
+              onClick={handleSignInWithGoogle}
               className="btn btn-outline border-none bg-[#ff8080] hover:bg-[#f16c6c] text-white"
             >
-              <FcGoogle className="text-2xl" /> <span className="ml-2">Sign up with Google</span>{" "}
+              <FcGoogle className="text-2xl" />{" "}
+              <span className="ml-2">Sign up with Google</span>{" "}
             </button>
           </form>
           <p className="text-center">
