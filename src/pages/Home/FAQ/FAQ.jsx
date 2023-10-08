f9343c76b13a6100c95da1df30935a9f072a1dcf
bikash-dev <bikash.dev.bk@gmail.com>
2023-10-08 21:28:41 +0600

    section description added in home page

--- src/pages/Home/FAQ/FAQ.jsx
+++ src/pages/Home/FAQ/FAQ.jsx
@@ -3,9 +3,10 @@ import faq from '../../../assets/faq.jpg'
 const FAQ = () => {
   return (
     <div className="max-w-7xl mx-auto my-32 p-5 md:p-4 lg:p-2">
-      <h2 className="text-4xl font-bold text-center mb-24">
+      <h2 className="text-4xl font-bold text-center">
         Frequently Asked Questions
       </h2>
+      <p className="max-w-4xl mx-auto text-center text-lg mt-5 mb-24">If you can't find the answers you're looking for in our FAQs, please don't hesitate to reach out to our dedicated team. We're here to assist you and ensure your wedding event is everything you've envisioned. Your satisfaction is our priority, and we're excited to be part of your special day.</p>
       <div className='flex gap-6 flex-col lg:flex-row items-center'>
         <div className='flex-1'>
           <img src={faq} alt="" className="rounded-tl-[60px] rounded-br-[60px] md:rounded-tl-[100px] md:rounded-br-[100px] border-4 border-[#8ECDDD] hover:shadow-2xl hover:shadow-[#8ECDDD]" />
